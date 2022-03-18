540500182afe2335fae62ab7fcbd88a6b4ade69f
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-03-18 22:13:20 +0000

    Add unit tests for even odd problem

--- src/main/java/org/kente/epi/EvenOdd.java
+++ src/main/java/org/kente/epi/EvenOdd.java
@@ -2,7 +2,21 @@ package org.kente.epi;
 
 public class EvenOdd {
 
-    public static void main(String[] args){
-        System.out.println("Solution to come");
+    public static int[] evenOdd(int[] array){
+        int nextEven = 0;
+        int nextOdd = array.length - 1;
+
+        while(nextEven < nextOdd){
+            if(array[nextEven] % 2 == 0){
+                nextEven++;
+            }else{
+                int temp = array[nextEven];
+                array[nextEven] = array[nextOdd];
+                array[nextOdd] = temp;
+                nextOdd--;
+            }
+        }
+
+        return array;
     }
 }
--- src/test/java/org/kente/epi/EvenOddTest.java
+++ src/test/java/org/kente/epi/EvenOddTest.java
@@ -1,4 +1,36 @@
 package org.kente.epi;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class EvenOddTest {
+
+    @Test
+    void givenEmptyArray_whenEvenOdd_thenReturnEmptyArray(){
+        int[] expected = new int[0];
+        int[] actual = EvenOdd.evenOdd(new int[0]);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void givenEvenIntegerArray_whenEvenOdd_thenReturnArrayInSameOrder(){
+        int[] expected = {2, 4, 6, 8};
+        int[] actual = EvenOdd.evenOdd(new int[]{2, 4, 6, 8});
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void givenOddIntegerArray_whenEvenOdd_thenReturnArrayInSameOrder(){
+        int[] expected = {3, 5, 7, 1};
+        int[] actual = EvenOdd.evenOdd(new int[]{1, 3, 5, 7});
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void givenEvenAndOddIntegerArray_whenEvenOdd_thenReturnArrayWithEvenNumbersAtStart(){
+        int[] expected = {8, 6, 2, 1, 7, 5, 3};
+        int[] actual = EvenOdd.evenOdd(new int[]{3, 5, 7, 1, 2, 6, 8});
+        assertArrayEquals(expected, actual);
+    }
 }
