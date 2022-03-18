70b5c07f2e001420176dd942fe53b27d2e5a8d09
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-03-18 21:41:09 +0000

    Create dummy files

---
+++ src/main/java/org/kente/epi/EvenOdd.java
@@ -0,0 +1,8 @@
+package org.kente.epi;
+
+public class EvenOdd {
+
+    public static void main(String[] args){
+        System.out.println("Solution to come");
+    }
+}
---
+++ src/test/java/org/kente/epi/EvenOddTest.java
@@ -0,0 +1,4 @@
+package org.kente.epi;
+
+public class EvenOddTest {
+}
